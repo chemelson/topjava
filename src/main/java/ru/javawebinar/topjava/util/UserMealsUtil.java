c6dfa2f552810caf453b6f9bd0d71910fc62daed
Anton Chemlev <ross03@mail.ru>
2019-05-27 12:32:44 +0300

    HW0: Optional 1 ready for review

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -23,6 +24,26 @@ public class UserMealsUtil {
                 meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         System.out.println(mealsWithExceed);
 
+        List<UserMealWithExceed> mealsWithExceedStream = getFilteredWithExceededStream(
+                meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        System.out.println(mealsWithExceedStream);
+    }
+
+    private static List<UserMealWithExceed> getFilteredWithExceededStream(
+            List<UserMeal> meals, LocalTime mealStartTime, LocalTime mealEndTime, int caloriesLimitPerDay) {
+        Map<LocalDate, Integer> caloriesForMealsInTimeByDate = meals
+                .stream()
+                .collect(Collectors.groupingBy(UserMeal::getDate, Collectors.summingInt(UserMeal::getCalories)));
+
+        return meals
+                .stream()
+                .filter(meal -> {
+                    LocalTime mealConsumedTime = meal.getTime();
+                    return mealConsumedTime.isAfter(mealStartTime) && mealConsumedTime.isBefore(mealEndTime);
+                })
+                .filter(meal -> caloriesForMealsInTimeByDate.getOrDefault(meal.getDate(), 0) > caloriesLimitPerDay)
+                .map(meal -> new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), true))
+                .collect(Collectors.toList());
     }
 
     private static List<UserMealWithExceed> getFilteredWithExceeded(
