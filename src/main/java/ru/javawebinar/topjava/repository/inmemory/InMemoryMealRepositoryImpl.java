feba671b3dc9de7c3c7c94e8b9d5a58251bd5d64
Anton Chemlev <ross03@mail.ru>
2019-06-18 17:26:52 +0300

    HW2: Ready for first review

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepositoryImpl.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -9,7 +12,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepositoryImpl implements MealRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepositoryImpl.class);
     private Map<Integer, List<Meal>> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
@@ -21,6 +26,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
+        log.info("save {}", meal);
         repository.computeIfAbsent(userId, key -> new ArrayList<>());
         List<Meal> meals = repository.get(userId);
         if (meal.isNew()) {
@@ -36,12 +42,14 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public boolean delete(int id, int userId) {
+        log.info("delete {}", id);
         List<Meal> meals = repository.get(userId);
         return meals.removeIf(meal -> meal.getId() == id);
     }
 
     @Override
     public Meal get(int id, int userId) {
+        log.info("get {}", id);
         List<Meal> meals = repository.get(userId);
         if (meals.isEmpty()) {
             return null;
@@ -51,7 +59,8 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
     }
 
     @Override
-    public Collection<Meal> getAll(int userId) {
+    public List<Meal> getAll(int userId) {
+        log.info("getAll for user {}", userId);
         return repository.get(userId).stream()
                 .sorted(Comparator.comparing(Meal::getDate).reversed()).collect(Collectors.toList());
     }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,53 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
 
+import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
+import static ru.javawebinar.topjava.web.SecurityUtil.authUserCaloriesPerDay;
+import static ru.javawebinar.topjava.util.MealsUtil.getWithExcess;
+
+import java.util.List;
+
+@Controller
 public class MealRestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
     private MealService service;
 
+    public List<MealTo> getAllByUser() {
+        int userId = authUserId();
+        log.info("getAll for user " + userId);
+        return getWithExcess(service.getAllByUser(userId), authUserCaloriesPerDay());
+    }
+
+    public Meal get(int id) {
+        int userId = authUserId();
+        log.info("get meal " + id + " for user " + userId);
+        return service.get(id, userId);
+    }
+
+    public Meal create(Meal meal) {
+        int userId = authUserId();
+        log.info("create meal for user " + userId);
+        return service.create(meal, userId);
+    }
+
+    public void delete(int id) {
+        int userId = authUserId();
+        log.info("delete meal " + id + " for user " + userId);
+        service.delete(id, userId);
+    }
+
+    public void update(Meal meal) {
+        int userId = authUserId();
+        log.info("update meal " + meal.getId() + " for user " + userId);
+        service.update(meal, userId);
+    }
 }
